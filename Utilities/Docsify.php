24901d14dc77701ddf8566f3d01748771715382f
Joshua Petry <jpetry@barkleyus.com>
2024-12-12 13:00:53 -0500

    2.2.6 - Docsify work: sidebar logic updates, initialization bugfix, display name adjustments - and composer update of 2.8.3 to 2.8.4

--- Utilities/Docsify.php
+++ Utilities/Docsify.php
@@ -5,6 +5,7 @@ namespace Barkley\CreatePhar\Utilities;
 use Barkley\CreatePhar\Config\ProjectConfig;
 use Barkley\CreatePhar\Utilities\Docs;
 use Barkley\CreatePhar\Utilities\Version;
+use InvalidArgumentException;
 use rei\CreatePhar\Output;
 
 require_once __DIR__.'/../Output.php';
@@ -74,19 +75,72 @@ class Docsify {
 		self::_CopyDirectoryAndContents('./docs', './docsify/docs');
 	}
 
+	private static function _RecursiveGlob($pattern, $flags = 0) {
+		$files = glob($pattern, $flags);
+		foreach (glob(dirname($pattern).'/*', GLOB_ONLYDIR|GLOB_NOSORT) as $dir) {
+			$files = array_merge(
+				[],
+				...[$files, self::_RecursiveGlob($dir . "/" . basename($pattern), $flags)]
+			);
+		}
+		return $files;
+	}
+
+	private static function _MultiExplode(array $delimiters, string $string) : array {
+		$ready = str_replace($delimiters, $delimiters[0], $string);
+		return explode($delimiters[0], $ready);
+	}
+
 	public static function BuildSidebar(string $version, ?string $createPharVersion = null, array $exclusions = ['README.md']) : void {
+
+		Output::Verbose('Building Sidebar...');
 
 		$contents = array();
 		$contents[] = '- [Overview](README)';
 
-		foreach(glob("./docsify/docs/*.md") as $file) {
-			if (is_file($file)) {
+		//foreach(self::_RecursiveGlob("./docsify/docs/*.md") as $file) {
+
+		$dir = './docsify/docs';
+		foreach (array_diff(scandir($dir), ['..','.']) as $filename) {
+
+			$file = "{$dir}/{$filename}";
+			Output::Verbose("\t{$file}");
+
+			if (is_file($file) && str_ends_with($file, '.md')) {
 				$bn = basename($file);
-				if (!in_array($bn, $exclusions)) {
-					$dn = str_replace('.md', '', $bn);
-					$dn = str_replace('.MD', '', $dn);
+				$bnWithDir = str_replace('./docsify/docs/', '', $file);
+				Output::Verbose("\t\t✅ File added to sidebar!");
+				if (!in_array($bnWithDir, $exclusions)) {
+					$dn = '';
+					// If the file is a nested README, treat it as the folder index
+					if (strtolower($bn) === 'readme.md') {
+						$e = self::_MultiExplode(['/', '\\'], $file);
+						$dn = $e[count($e)-2];
+					} else {
+						$dn = self::_GetDisplayName($bn);
+					}
 					$contents[] = "- [{$dn}](docs/{$bn})";
 				}
+			} elseif (is_dir($file) && !in_array($filename, ['img'])) {
+
+				Output::Verbose("\t\tIs Directory");
+
+				// Look for directory index
+				if (is_file("$file/README.md")) {
+					$contents[] = "- [{$filename}](docs/{$filename}/README.md)";
+					Output::Verbose("\t\t✅ Added index file for {$filename}");
+				} else {
+					Output::Error("Docsify directory {$file} does not have a README.md index file.");
+				}
+
+				foreach (array_diff(scandir($file), ['..','.', 'README.md']) as $nestedFilename) {
+					if (is_file("./docsify/docs/{$filename}/{$nestedFilename}")) {
+						Output::Verbose("\t\t✅ Nested File: {$nestedFilename}");
+						$dn = self::_GetDisplayName($nestedFilename);
+						$contents[] = "  - [{$dn}](docs/{$filename}/{$nestedFilename})";
+					}
+				}
+
 			}
 		}
 
@@ -103,6 +157,14 @@ class Docsify {
 
 	}
 
+	private static function _GetDisplayName(string $filename) : string {
+		$dn = str_replace('.md', '', $filename);
+		$dn = str_replace('.MD', '', $dn);
+		$dn = str_replace(['-','_'], ' ', $dn);
+
+		return $dn;
+	}
+
 	private static function _CopyDirectoryAndContents(string $source, string $destination, array $exclusions = ['README.md']) : void {
 
 		if (!is_dir($source)) {
@@ -114,8 +176,9 @@ class Docsify {
 		foreach(glob("{$source}/*") as $file) {
 			//Output::Info('Copying...'.$file);
 			if (is_dir($file)) {
-				self::_CopyDirectoryAndContents($file, $destination.'/'.basename($file));
+				self::_CopyDirectoryAndContents($file, $destination.'/'.basename($file), []);
 			} else {
+
 				if (!in_array(basename($file), $exclusions)) {
 					copy($file, $destination . '/' . basename($file));
 				}
@@ -125,10 +188,30 @@ class Docsify {
 
 	}
 
-	private static function _MakeDir(string $dir) : void {
-		if (!is_dir($dir)) {
-			mkdir($dir);
+	private static function _DeleteDir(string $dir) : void {
+		if (! is_dir($dir)) {
+			throw new InvalidArgumentException("$dir must be a directory");
 		}
+		if (substr($dir, strlen($dir) - 1, 1) != '/') {
+			$dir .= '/';
+		}
+		$files = glob($dir . '*', GLOB_MARK);
+		foreach ($files as $file) {
+			if (is_dir($file)) {
+				self::_DeleteDir($file);
+			} else {
+				unlink($file);
+			}
+		}
+		rmdir($dir);
+	}
+
+	private static function _MakeDir(string $dir) : void {
+		if (is_dir($dir)) {
+			self::_DeleteDir($dir);
+		}
+
+		mkdir($dir);
 	}
 
 }
